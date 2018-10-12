7c5d5089451875cde58f2047d0e84e465c829a95
Julian <julianboyd0@gmail.com>
2018-10-12 15:06:17 -0500

    Schema established

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+app.get('/', (req, res) => {
+    console.log('Its alive')
+    res.send('Its alive')
+})
+
 
 app.listen(3000, () => {
   console.log('andre 3000 listening')
